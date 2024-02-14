da7a954e49003d318806213b598681179adb98ab
Muavia Asghar <newgamer445@gmail.com>
2024-02-14 18:09:41 +0500

    pyramid pattern

--- pyramidPatternWithAestrik.dart
+++ pyramidPatternWithAestrik.dart
@@ -10,14 +10,11 @@ void main() {
   print("Enter the number of rows for the pyramid:");
   int numRows = int.parse(stdin.readLineSync()!);
 
-  print("Pyramid pattern with asterisks:");
   for (int i = 1; i <= numRows; i++) {
-    // Print spaces for alignment
     for (int space = 1; space <= numRows - i; space++) {
       stdout.write(' ');
     }
 
-    // Print asterisks
     for (int j = 1; j <= i; j++) {
       stdout.write('* ');
     }
