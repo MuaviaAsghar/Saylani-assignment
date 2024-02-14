6eb5859f4b7c208a7ba9b8a32cfb031957550db5
Muavia Asghar <newgamer445@gmail.com>
2024-02-14 17:26:52 +0500

    rtTriangle with numbers

--- rtAngleTriangleWithNumbers.dart
+++ rtAngleTriangleWithNumbers.dart
@@ -1,4 +1,4 @@
-// Q 10: Program to display a pattern like a right angle triangle using an asterisk using loop.
+// Q 11: Program to display a pattern like a right angle triangle with a number using loop.
 
 import 'dart:io';
 
@@ -6,10 +6,11 @@ void main() {
   print("Enter the number of rows for the right-angled triangle:");
   int numRows = int.parse(stdin.readLineSync()!);
 
+  print("Right-angled triangle pattern with numbers:");
   for (int i = 1; i <= numRows; i++) {
     for (int j = 1; j <= i; j++) {
-      stdout.write('*');
+      stdout.write('$j');
     }
-    print(' ');
+    print('');
   }
 }
